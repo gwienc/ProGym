32398322d6483d2073dd59ad82e6319af460f844
gwienc <grzegorz.wienc5@gmail.com>
2021-09-25 11:04:00 +0200

    1. Zainstalowanie EntityFramework 2. Utworzenie klasy StoreContext odpowiedzialnej za utworzenie bazy danych

---
+++ ProGym/DAL/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using ProGym.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProGym.DAL
+{
+    public class StoreContext :DbContext
+    {
+
+        public StoreContext() :base("StoreContext")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
