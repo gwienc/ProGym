83f274896b07e80dc681b43d7dabc9fe764c7b15
gwienc <grzegorz.wienc5@gmail.com>
2021-09-26 11:24:59 +0200

    Dodanie Customowego Helpera do pobierania zdjęć produktów

---
+++ ProGym/Infrastructure/AppConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ProGym.Infrastructure
+{
+    public class AppConfig
+    {
+        private static string _photosFolder = ConfigurationManager.AppSettings["PhotosFolder"];
+
+        public static string PhotosFolder
+        {
+            get
+            {
+                return _photosFolder;
+            }
+        }
+
+    }
+}
---
+++ ProGym/Infrastructure/UrlHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProGym.Infrastructure
+{
+    public static class UrlHelpers
+    {
+        public static string ProductPhotoPath(this UrlHelper helper, string productFileName )
+        {
+            var productPhotoFolder = AppConfig.PhotosFolder;
+            var path = Path.Combine(productPhotoFolder, productFileName);
+            var absolutePath = helper.Content(path);
+            return absolutePath;
+        }
+    }
+}
