2bf3f049c3b31f37d061732dcded07b4497b53b4
gwienc <grzegorz.wienc5@gmail.com>
2021-10-06 18:53:42 +0200

    Utworzenie paczki plików css wykorzystanych podczas działania wyszukiwarki

--- ProGym/App_Start/BundleConfig.cs
+++ ProGym/App_Start/BundleConfig.cs
@@ -8,9 +8,14 @@ namespace ProGym.App_Start
 {
     public class BundleConfig
     {
-        public static void RegisterBundle(BundleCollection bundes)
+        public static void RegisterBundle(BundleCollection bundles)
         {
-            bundes.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/content/themes/base/css").Include("~/Content/themes/base/core.css",
+                                                                             "~/Content/themes/base/autocomplete.css",
+                                                                             "~/Content/themes/base/theme.css",
+                                                                             "~/Content/themes/base/menu.css"));
         }
     }
 }
