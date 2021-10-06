6ed180c9e7b72c4819537bb2da5f2ee4512ee149
gwienc <grzegorz.wienc5@gmail.com>
2021-10-06 18:45:24 +0200

    1. Instalacja pakietu Web.Optimization 2. Uworzenie klasy BundleConfig, w której dodana została skompresowana paczka ze skryptami jquery 3. W widoku Index kontrolera Store na dole dodana została referencja do utworzonej paczki skryptów 4. Zarejestrowanie paczki skryptów w pliku Global.asax w celu wywołania jej podczas startu aplikacji

---
+++ ProGym/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ProGym.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundes)
+        {
+            bundes.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+        }
+    }
+}
--- ProGym/Global.asax.cs
+++ ProGym/Global.asax.cs
@@ -1,8 +1,10 @@
+using ProGym.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ProGym
@@ -13,6 +15,7 @@ namespace ProGym
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundle(BundleTable.Bundles);
         }
     }
 }
