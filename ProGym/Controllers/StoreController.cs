8f3d3b7ce34c32720db372e410517e5d9272f1e8
grzegorz.wienc5 <grzegorz.wienc5@gmail.com>
2021-10-07 12:59:14 +0200

    1. Zmiany w metodzie akcji Index kontrolera Store, 2. Zmiany w widoku _ProductList 3. Zmiana w stylizacji wyszukiwarki 4.Zmiana w widoku Index

--- ProGym/Controllers/StoreController.cs
+++ ProGym/Controllers/StoreController.cs
@@ -11,28 +11,39 @@ namespace ProGym.Controllers
     {
         StoreContext db = new StoreContext();
 
-        public ActionResult Index(string categoryname = "Wszystkie", string searchQuery = null)
+        public ActionResult Index(string categoryname, string searchQuery = null)
         {
-            if (categoryname == "Wszystkie" && searchQuery == null)
+            if ((categoryname == null || categoryname == "Wszystkie") && searchQuery == null)
             {
                 var products = db.Products.ToList();
                 return View(products);
             }
+
+            else if ((categoryname == "Wszystkie" || categoryname == null) && searchQuery != null)
+            {
+                var products = db.Products.Where(p => (p.Name.ToLower().Contains(searchQuery.ToLower()) || p.ProducerName.ToLower().Contains(searchQuery.ToLower())) && !p.IsHidden);
+
+                if (Request.IsAjaxRequest())
+                {
+                    return PartialView("_ProductList", products);
+                }
+                return View(products);
+            }        
             else
             {
                 var category = db.Categories.Include("Products").Where(c => c.CategoryName.ToUpper() == categoryname.ToUpper()).Single();
                 var products = category.Products.Where(p => (searchQuery == null || p.Name.ToLower().Contains(searchQuery.ToLower()) || p.ProducerName.ToLower().Contains(searchQuery.ToLower())) && !p.IsHidden);
 
-                if(Request.IsAjaxRequest())
+                if (Request.IsAjaxRequest())
                 {
                     return PartialView("_ProductList", products);
                 }
                 return View(products);
             }
-         
+
         }
 
-  
+
         public ActionResult Details(int id)
         {
             var product = db.Products.Find(id);
@@ -40,11 +51,11 @@ namespace ProGym.Controllers
         }
 
         [ChildActionOnly]
-        [OutputCache(Duration =86400)]
+        [OutputCache(Duration = 86400)]
         public ActionResult CategoriesMenu()
         {
             var categories = db.Categories.ToList();
-            return PartialView("_CategoriesMenu",categories);
+            return PartialView("_CategoriesMenu", categories);
         }
 
 
