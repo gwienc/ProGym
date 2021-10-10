4be1d56857d2cf4e5a9023d7f5ee0250897784cc
gwienc <grzegorz.wienc5@gmail.com>
2021-10-10 11:49:33 +0200

    1. Utworzenie view modelu CartViewModel odpowiedzialnego za przekazanie informacji do koszyka 2. Wstępna implementacja widoku koszyka z danymi z bazy danych

---
+++ ProGym/ViewModels/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ProGym.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProGym.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
