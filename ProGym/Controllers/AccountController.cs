b699fb52b0100ec515895d7dbacb1b735e1b7d21
gwienc <grzegorz.wienc5@gmail.com>
2021-10-14 19:22:25 +0200

    1. Dodanie pakietu NuGet Microsoft.Owin.Host.SystemWeb 2. Zmiany w AccountController

--- ProGym/Controllers/AccountController.cs
+++ ProGym/Controllers/AccountController.cs
@@ -1,15 +1,52 @@
-﻿using ProGym.ViewModels;
+﻿using ProGym.App_Start;
+using ProGym.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace ProGym.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+        
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get => _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        private ApplicationSignInManager _signInManager;
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
         public ActionResult Login(string ReturnUrl)
         {
             ViewBag.ReturnUrl = ReturnUrl;
