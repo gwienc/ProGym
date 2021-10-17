8efc3dff99c9dad417bbe6b065ef89f828360973
gwienc <grzegorz.wienc5@gmail.com>
2021-10-17 12:46:20 +0200

    1. Dodanie kontrolera Manage 2. Dodanie adnotacji do klasy UserData 3. Dodanie view modelu ManageViewModels 4. Dodanie widoku Index do metody Index kontrolera Manage

--- ProGym/Models/UserData.cs
+++ ProGym/Models/UserData.cs
@@ -8,19 +8,27 @@ namespace ProGym.Models
 {
     public class UserData
     {
+        
+        [Display(Name = "Imię:")]
         public string FirstName { get; set; }
-
+        
+        [Display(Name = "Nazwisko:")]
         public string LastName { get; set; }
-
+        
+        [Display(Name = "Ulica i nr domu/mieszkania:")]
         public string Address { get; set; }
-
+        
+        [Display(Name = "Miasto i kod pocztowy:")]
         public string CodeAndCity { get; set; }
 
+        
         [RegularExpression(@"(\+\d{2})*[\d\s-]+",
             ErrorMessage = "Błędny format numeru telefonu.")]
+        [Display(Name = "Numer telefonu:")]
         public string PhoneNumber { get; set; }
 
         [EmailAddress(ErrorMessage = "Błędny format adresu e-mail.")]
+        [Display(Name = "E-mail:")]
         public string Email { get; set; }
     }
 }
---
+++ ProGym/ViewModels/ManageViewModels.cs
@@ -0,0 +1,39 @@
+﻿using ProGym.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProGym.Models
+{
+    public class ManageCredentialsViewModel
+    {
+        public bool HasPassword { get; set; }
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public ProGym.Controllers.ManageController.ManageMessageId? Message { get; set; }
+        public bool ShowRemoveButton { get; set; }
+
+        public UserData UserData { get; set; }
+    }
+
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Aktualne hasło")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "{0} musi zawierać minimum {2} znaków, co najmniej jeden znak inny niż litera lub cyfra,co najmniej jedną cyfrę („0”-„9”), co najmniej jedną wielką literę („A”-„Z”).”", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nowe hasło")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Powtórz nowe hasło:")]
+        [Compare("NewPassword", ErrorMessage = "Podane hasło nie jest identyczne")]
+        public string ConfirmPassword { get; set; }
+    }
+}
