8e4e3754cc493209c42c5d4dc472a79e72716e6d
gwienc <grzegorz.wienc5@gmail.com>
2021-10-21 16:53:43 +0200

    1. Dodanie adnotacji walidacyjnych do klas Product, AddOrEditProductViewModel i Category 2. Zmiany w widokacj AddProduct, OrdersList i Details 3. Zmiany stylizacyjne w pliku Site.css

--- ProGym/Models/Product.cs
+++ ProGym/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +10,35 @@ namespace ProGym.Models
     {
         public int ProductID { get; set; }
         public int CategoryID { get; set; }
+        [Display(Name = "Nazwa produktu")]
+        [Required(ErrorMessage = "Proszę podać nazwę produktu")]
         public string Name { get; set; }
+        [Display(Name = "Nazwa producenta")]
+        [Required(ErrorMessage = "Proszę podać nazwę producenta")]
         public string ProducerName { get; set; }
+
+        [Display(Name = "Cena")]
+        [Required(ErrorMessage = "Proszę podać cenę")]
         public decimal Price { get; set; }
+        
+        [Required(ErrorMessage = "Proszę podać skład")]
+        [Display(Name = "Skład")]
         public string Ingredients { get; set; }
+        
+        [Required(ErrorMessage = "Proszę podać opis")]
+        [Display(Name = "Opis")]
         public string Description { get; set; }
+        
+        [Display(Name = "Skrócony opis")]
+        [Required(ErrorMessage = "Proszę podać skrócony opis")]
         public string ShortDescription { get; set; }
+        
+        [Required(ErrorMessage = "Proszę podać parametry")]
+        [Display(Name = "Parametry")]
         public string Parameters { get; set; }
+        
+        [Display(Name = "Zdjęcie")]
+        [Required(ErrorMessage = "Proszę wczytać zdjęcie")]
         public string PhotoFileName { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsHidden { get; set; }
--- ProGym/ViewModels/AddOrEditProductViewModel.cs
+++ ProGym/ViewModels/AddOrEditProductViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ProGym.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace ProGym.ViewModels
     public class AddOrEditProductViewModel
     {
         public Product Product { get; set; }
+        [Display(Name = "Kategoria")]
         public IEnumerable<Category> Categories { get; set; }
         public bool? ConfirmSuccess { get; set; }
     }
