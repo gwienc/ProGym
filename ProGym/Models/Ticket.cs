892937494a21d548e0e581a4eadb056506a61dd7
gwienc <grzegorz.wienc5@gmail.com>
2021-11-01 12:56:01 +0100

    Dodanie klas modelu Ticket oraz TypeOfTicket

---
+++ ProGym/Models/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProGym.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public string UserId { get; set; }
+
+        [Display(Name = "Data zakupu")]
+        public DateTime DateOfPurchase { get; set; }
+
+        [Display(Name = "Data ważności")]
+        public DateTime ExpirationDate { get; set; }
+
+        [Display(Name = "Rodzaj karnetu")]
+        public TypeOfTicket TypeOfTicket { get; set; }
+
+        [Display(Name = "Ważność")]
+        public bool IsActive { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ ProGym/Models/TypeOfTicket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProGym.Models
+{
+    public class TypeOfTicket
+    {
+        public int TypeOfTicketId { get; set; }
+        public TypeTicket TypeTicket { get; set; }
+        public PeriodOfValidity PeriodOfValidity { get; set; }
+
+    }
+
+    public enum TypeTicket
+    {
+        Student,
+        Open,
+        OpenMax
+    }
+
+    public enum PeriodOfValidity
+    {
+        [Display(Name = "Miesiąc")]
+        OneMonth,
+
+        [Display(Name = "3 miesiące")]
+        ThreeMonth,
+
+        [Display(Name = "6 miesięcy")]
+        SixMonth
+    }
+}
