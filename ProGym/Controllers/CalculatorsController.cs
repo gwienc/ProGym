669442c5657cce8b1ca2257693b574d361a6fe0d
gwienc <grzegorz.wienc5@gmail.com>
2021-11-14 19:23:01 +0100

    1. Dodanie nowego kontrolera Calculators 2. Zainstalowanie pakietu NuGet Microsoft.jQuery.Unobtrusive.Ajax 3. Implementacja kalkulatora BMI, który za pomocą technologi Ajax oblicza oraz wyświetla wynik

---
+++ ProGym/Controllers/CalculatorsController.cs
@@ -0,0 +1,61 @@
+﻿using ProGym.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProGym.Controllers
+{
+    public class CalculatorsController : Controller
+    {
+        // GET: Calculators
+        public ActionResult Index()
+        {          
+            return View();
+        }
+
+        public PartialViewResult CalculateBMI(BMIViewModel model)
+        {
+            model.Height = model.Height * 0.01;
+            model.Result = model.Weight / (model.Height*model.Height);
+
+            if (model.Result < 16)
+            {
+                model.Range = "Wyglodzenie";
+                
+            }
+            else if (model.Result > 16 && model.Result < 16.99)
+            {
+                model.Range = "Wychudzenie";
+            }
+            else if (model.Result > 17 && model.Result < 18.49)
+            {
+                model.Range = "Niedowaga";
+            }
+            else if (model.Result > 18.5 && model.Result < 24.99)
+            {
+                model.Range = "Prawidłowa masa ciała";
+            }
+            else if (model.Result > 25 && model.Result < 29.99)
+            {
+                model.Range = "Nadwaga";
+            }
+            else if (model.Result > 30 && model.Result < 34.99)
+            {
+                model.Range = "Otyłość I stopnia";
+            }
+            else if (model.Result > 35 && model.Result < 39.99)
+            {
+                model.Range = "Otyłość II stopnia";
+            }
+            else if (model.Result >= 40)
+            {
+                model.Range = "Otyłość III stopnia chorobliwa";
+            }
+
+            return PartialView("_CalculateBMI", model);
+
+        }
+    }
+}
---
+++ ProGym/ViewModels/BMIViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProGym.ViewModels
+{
+    public class BMIViewModel
+    {
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public double Result { get; set; }
+        public string Range { get; set; }
+    }
+}
