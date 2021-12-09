712dbec1c56244f8ae9c7b4d813c715dc2a96ea1
grzegorz.wienc5 <grzegorz.wienc5@gmail.com>
2021-12-09 19:48:24 +0100

    Dodanie kilku dodatkowych tras routingu w pliku RouteConfig

--- ProGym/App_Start/RouteConfig.cs
+++ ProGym/App_Start/RouteConfig.cs
@@ -13,13 +13,59 @@ namespace ProGym
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-           
+
+            routes.MapRoute(
+                name: "Workouts",
+                url: "ProGym/Workouts",
+                defaults: new { controller = "Workout", action = "Index" }
+                );
+
+            routes.MapRoute(
+                name: "Calculators",
+                url: "ProGym/Calculators",
+                defaults: new { controller = "Calculators", action = "Index" }
+                );
+
+            routes.MapRoute(
+                name: "BuyTicket",
+                url: "ProGym/Tickets/{typeTicket}/{periodOfValidity}/{ticketId}",
+                defaults: new { controller = "Ticket", action = "BuyTicket" }
+                );
+
+            routes.MapRoute(
+                name: "ChooseTicket",
+                url: "ProGym/Tickets/Ticket-{type}",
+                defaults: new { controller = "Ticket", action = "ChooseTicket" }
+                );
+
+            routes.MapRoute(
+                name: "Tickets",
+                url: "ProGym/Tickets",
+                defaults: new { controller = "Ticket", action = "Index" }
+                );
+
+            routes.MapRoute(
+               name: "ProductList",
+               url: "ProGym/Store/Category/{categoryname}",
+               defaults: new { controller = "Store", action = "Index" }
+               );
             routes.MapRoute(
                 name: "ProductDetalis",
-                url: "{controller}/product-{id}",
+                url: "ProGym/Store/product-{id}",
                 defaults: new { controller = "Store", action = "Details" }
                 );
 
+            routes.MapRoute(
+                name: "Store",
+                url: "ProGym/Store",
+                defaults: new { controller = "Store", action = "Index" }
+                );
+
+            routes.MapRoute(
+                name: "ChangeProfile",
+                url: "ProGym/Manage/{action}",
+                defaults: new { controller = "Manage", action = "Index" }
+                );        
 
             routes.MapRoute(
                 name: "StaticPages",
@@ -27,13 +73,11 @@ namespace ProGym
                 defaults: new { controller = "Home", action = "StaticContent" }
                 );
 
-
             routes.MapRoute(
-                name: "ProductList",
-                url: "{controller}/Kategoria/{categoryname}",
-                defaults: new { controller = "Store", action = "Index" }
+                name: "HomePage",
+                url: "ProGym",
+                defaults: new { controller = "Home", action = "Index" }
                 );
-
 
             routes.MapRoute(
                 name: "Default",
